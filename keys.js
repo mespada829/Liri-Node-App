a27b3b5c887f9565db8af4217b57ed0410a2d8fa
Marita Espada <MaritaEspada@Maritas-Air.fios-router.home>
2017-06-21 15:08:32 -0400

    last

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'squkevbrnU5h92cziqUbETnOO',
+  consumer_secret: 'egpuEQG8oXmYsKhxaCXDxsz0i1mt7XNzWySHgyAqsOBu5p55pv',
+  access_token_key: '873249017735651328-pO04cm8Mgv5TQMDxIvhjSXVvUwck824',
+  access_token_secret: 'txfn4IlBwPqMmFuoFQLQCFYCQ8zGOsQ3uvBCAhXvXKHfP',
+  
+}
---
+++ liri.js
@@ -0,0 +1,39 @@
+// Require packages 
+
+var fs = require('fs')
+
+var Twitter = require('twitter');
+
+
+//  Variable
+
+var tweets = "my-tweets";
+
+// Twitter Function 
+function mytweets () {
+// Code from NPM
+
+var client = new Twitter({
+    consumer_key: 'squkevbrnU5h92cziqUbETnOO',
+    consumer_secret: 'egpuEQG8oXmYsKhxaCXDxsz0i1mt7XNzWySHgyAqsOBu5p55pv',
+    access_token_key: '873249017735651328-pO04cm8Mgv5TQMDxIvhjSXVvUwck824',
+    access_token_secret: 'txfn4IlBwPqMmFuoFQLQCFYCQ8zGOsQ3uvBCAhXvXKHfP'
+});
+
+var params = { screen_name: 'Sword' };
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+        console.log(error);
+
+
+
+ //Display Tweets
+  for (var i=0; i<tweets.length; i++){
+    console.log("Tweet: " + tweets[i].text);
+  }
+
+
+}}
+    
+});
+
